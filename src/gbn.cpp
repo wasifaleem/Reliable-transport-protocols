18b33ef47c8213e169b0149f8ff8d4b0fdb4b372
Wasif Aleem <wasifaleem30@gmail.com>
2016-03-19 02:57:35 -0400

    GBN complete?

--- src/gbn.cpp
+++ src/gbn.cpp
@@ -20,7 +20,7 @@
 
 /********* STUDENTS WRITE THE NEXT SEVEN ROUTINES *********/
 
-#define DEBUG_LOG(AorB, str) do { std::cout << std::setprecision(5) << std::setw(8) << get_sim_time() << " : " << AorB << " : " << str << std::endl; } while( false )
+#define DEBUG_LOG(AorB, str) do { std::cout << std::setprecision(5) << std::setw(8) << get_sim_time() << "T: " << std::setw(3) << __LINE__  << "L: " << AorB << " : " << str << std::endl; } while( false )
 #define DEBUG_A(str) DEBUG_LOG('A', str)
 #define DEBUG_B(str) DEBUG_LOG('B', str)
 
@@ -28,34 +28,33 @@ std::ostream &operator<<(std::ostream &, const pkt &);
 
 std::ostream &operator<<(std::ostream &, const msg &);
 
-static const float timeout = 12.0; // TODO: RTT ?
+pkt make_pkt(int seq, int ack, const struct msg *msg);
 
-// A
-static std::vector<pkt *> sndpkt(1000);
-static std::queue<msg> buffer;
-static int base = 1, nextseqnum = 1, N;
+pkt make_ack(int ack);
 
-void trySendBuffered();
+int make_checksum(const struct pkt &pkt);
 
-// B
-static volatile int expectedseqnum = 1;
-
-pkt *make_pkt(int seq, int ack, struct msg *msg);
-
-pkt *make_ack(int ack);
-
-int make_checksum(struct pkt *pkt);
-
-bool is_corrupt(struct pkt pkt);
+bool is_corrupt(const struct pkt &pkt);
 
 float expected_timeout();
 
+static const float timeout = 12.0; // TODO: RTT ?
+
+// A
+static std::vector<pkt> sndpkt(1000);
+static std::queue<msg> buffer;
+static int base = 1, nextseqnum = 1, N;
+
+void send_buffered();
+
+// B
+static volatile int expectedseqnum = 1;
 
 /* called from layer 5, passed the data to be sent to other side */
 void A_output(struct msg message) {
     if (nextseqnum < base + N) {
         sndpkt[nextseqnum] = make_pkt(nextseqnum, 0, &message);
-        tolayer3(0, *sndpkt[nextseqnum]);
+        tolayer3(0, sndpkt[nextseqnum]);
         if (base == nextseqnum) {
             starttimer(0, expected_timeout());
         }
@@ -72,10 +71,10 @@ void A_input(struct pkt packet) {
     if (!is_corrupt(packet)) {
         if (packet.acknum == base) {
             DEBUG_A("Receive ACK: " << packet);
-            base = packet.acknum + 1; // TODO: free ack'd pkt, handle duplicate
+            base = packet.acknum + 1;
             if (base == nextseqnum) {
                 stoptimer(0);
-                trySendBuffered();
+                send_buffered();
             }
         } else {
             DEBUG_A("Receive DUP-ACK: " << packet);
@@ -85,11 +84,11 @@ void A_input(struct pkt packet) {
     }
 }
 
-void trySendBuffered() {
+void send_buffered() {
     while (!buffer.empty() && nextseqnum < base + N) {
         struct msg message = buffer.front();
         sndpkt[nextseqnum] = make_pkt(nextseqnum, 0, &message);
-        tolayer3(0, *sndpkt[nextseqnum]);
+        tolayer3(0, sndpkt[nextseqnum]);
         if (base == nextseqnum) {
             starttimer(0, expected_timeout());
         }
@@ -101,13 +100,13 @@ void trySendBuffered() {
 
 /* called when A's timer goes off */
 void A_timerinterrupt() {
-    DEBUG_A("Start Timer interrupt");
+    DEBUG_A("\033[31;1m" << "Start Timer interrupt" << "\033[0m");
     starttimer(0, expected_timeout());
-    for (int i = base; (i < nextseqnum && sndpkt[i] != NULL); ++i) {
-        DEBUG_A("Re-Sending: " << (*sndpkt[i]));
-        tolayer3(0, *sndpkt[i]);
+    for (int i = base; (i < nextseqnum); ++i) {
+        DEBUG_A("Re-Sending: " << (sndpkt[i]));
+        tolayer3(0, sndpkt[i]);
     }
-    DEBUG_A("End Timer interrupt");
+    DEBUG_A("\033[31;1m" << "End Timer interrupt" << "\033[0m");
 }
 
 /* the following routine will be called once (only) before any other */
@@ -121,20 +120,18 @@ void A_init() {
 /* called from layer 3, when a packet arrives for layer 4 at B*/
 void B_input(struct pkt packet) {
     if (!is_corrupt(packet) && packet.seqnum == expectedseqnum) {
-        DEBUG_B("Received: " << packet);
+        DEBUG_B("\033[32;1m" << "Received: " << packet << "\033[0m");
         tolayer5(1, packet.payload);
 
-        struct pkt *ack = make_ack(expectedseqnum);
-        DEBUG_B("Sending ACK: " << *ack);
-        tolayer3(1, *ack);
-        delete ack;
+        struct pkt ack = make_ack(expectedseqnum);
+        DEBUG_B("Sending ACK: " << ack);
+        tolayer3(1, ack);
 
         expectedseqnum++;
     } else {
         DEBUG_B("Re-sending ACK: " << expectedseqnum - 1);
-        struct pkt *ack = make_ack(expectedseqnum - 1);
-        tolayer3(1, *ack);
-        delete ack;
+        struct pkt ack = make_ack(expectedseqnum - 1);
+        tolayer3(1, ack);
     }
 }
 
@@ -154,33 +151,31 @@ float expected_timeout() {
 }
 
 
-pkt *make_pkt(int seq, int ack, struct msg *msg) {
-    struct pkt *pkt = new struct pkt();
-    pkt->seqnum = seq;
-    pkt->acknum = ack;
+pkt make_pkt(int seq, int ack, const struct msg *msg) {
+    struct pkt pkt = {.seqnum = seq, .acknum = ack};
     if (msg != NULL) {
-        memcpy(pkt->payload, (*msg).data, 20);
+        memcpy(pkt.payload, (*msg).data, 20);
     }
-    pkt->checksum = make_checksum(pkt);
+    pkt.checksum = make_checksum(pkt);
     return pkt;
 }
 
-int make_checksum(struct pkt *pkt) {
+int make_checksum(const struct pkt &pkt) {
     int checksum = 0;
-    checksum += pkt->seqnum;
-    checksum += pkt->acknum;
+    checksum += pkt.seqnum;
+    checksum += pkt.acknum;
     for (int i = 0; i < 20; ++i) {
-        checksum += pkt->payload[i];
+        checksum += pkt.payload[i];
     }
     return checksum;
 }
 
 
-bool is_corrupt(struct pkt pkt) {
-    return make_checksum(&pkt) != pkt.checksum;
+bool is_corrupt(const struct pkt &pkt) {
+    return make_checksum(pkt) != pkt.checksum;
 }
 
-pkt *make_ack(int ack) {
+pkt make_ack(int ack) {
     return make_pkt(0, ack, NULL);
 }
 
